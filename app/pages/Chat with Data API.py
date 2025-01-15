62a55a6e7ad53b41ab0c183b5aa8f626f0d25a1a
Michal@MacBook <michal.marusan@gmail.com>
2025-01-15 09:10:05 +0100

    added citations display and formatting

--- app/pages/Chat with Data API.py
+++ app/pages/Chat with Data API.py
@@ -89,7 +89,7 @@ with st.sidebar:
 
     st.session_state.model = st.selectbox("Select a model", AZURE_OPENAI_MODEL_NAME_LIST)
     st.session_state.temperature = st.slider("Temperature", 0.0, 1.0, 0.5, 0.01)
-    st.session_state.max_tokens = st.slider("Max tokens", 10, 4000, 400, 5)
+    st.session_state.max_tokens = st.slider("Max tokens", 10, 4000, 800, 5)
 
 
     # Display the text area with the initial value
@@ -149,12 +149,49 @@ if prompt := st.chat_input("What is up?"):
             #     )
             response = doc_utils.do_rag(messages=st.session_state.messages, stream=True, deployment=st.session_state.model, temperature=st.session_state.temperature, max_tokens=st.session_state.max_tokens, system_prompt=st.session_state.SYSTEM_PROMPT)
             
+            citations = []
             for part in response:
                 full_response += part.choices[0].delta.content or ""
+                try:
+                    _citations =  part.choices[0].delta.model_extra["context"]["citations"]
+                    citations.extend(_citations)
+                except:
+                    pass
                 message_placeholder.markdown(full_response + "▌")
+                
 
             # final response
             message_placeholder.markdown(full_response)
+
+            if len(citations) > 0:
+                with st.expander("Citations:", expanded=False):
+                    # st.write(citations)
+                    
+                    # extract from the content all [doc1] strings using regex
+                    import re
+                    # full_response = "Tato hodnota bodu se vztahovala na odbornosti VPL [doc5] (všeobecní praktičtí lékaři) i PLDD (praktici [doc1] pro děti a dorost) [doc1]. [doc3]"
+                    _docs = re.findall(r"\[doc\d+\]", full_response)
+                    # _docs is a list of all the matches with duplicates, remove duplicates
+                    _set_docs = set(_docs)
+                    # convert to list    
+                    _set_docs = list(_set_docs)
+                    # sort the list
+                    _set_docs.sort()
+                    # print(_set_docs)
+                    # extract from _set_docs the number of the document
+                    _doc_nums = []
+                    for doc in _set_docs:
+                        _doc_num = int(re.findall(r"\d+", doc)[0]) - 1 # -1 because the list is 0 based
+                        _doc_nums.append(_doc_num)
+                    for doc_num in _doc_nums:
+                        _content = citations[doc_num]["content"]
+                        if len(_content) > 300:
+                            _content = _content[:300] + "..."
+                        _title = citations[doc_num]["title"]
+                        _url = citations[doc_num]["url"]
+                        _chunk = citations[doc_num]["chunk_id"]
+                        st.markdown(f"📄 [doc{doc_num+1}] [ {_title}]({_url})")
+                        st.caption(f"{_content}")
 
             # add assistant response to chat history
             st.session_state.messages.append({"role": "assistant", "content": full_response})
